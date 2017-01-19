bed1c4e8f7dba3512b93e49fde8e46c4ebcd07ab
TMerrick55 <tmerrick44@gmail.com>
2017-01-19 10:27:34 -0500

    Test Functions Redo Ver. 5

--- testfunctions.rb
+++ testfunctions.rb
@@ -6,15 +6,19 @@ class TestMMFunctions < Minitest::Test
 		assert_equal(1,1)
 	end
 
-	def test_1_to_mined_minds_function_returns_1
+	def test_pass_1_to_mined_minds_function_returns_1
 		assert_equal(1, mined_minds_function(1))
 	end
 
-	def test_2_to_mined_minds_function_returns_2
+	def test_pass_2_to_mined_minds_function_returns_2
 		assert_equal(2, mined_minds_function(2))
 	end
 
-	def test_3_to_mined_minds_function_returns_mind
+	def test_pass_3_to_mined_minds_function_returns_mind
 		assert_equal("mined", mined_minds_function(3))
+	end
+
+	def test_pass_4_to_mined_minds_function_returns_mind
+		assert_equal("mined", mined_minds_function(9))
 	end
 end
